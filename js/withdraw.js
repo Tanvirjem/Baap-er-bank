c53057ace353ad8d13228bc9b20cced2cd83625d
Tanvirjem <tanviranjumshozib@gmail.com>
2023-02-19 16:23:01 +0600

    first commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,20 +4,32 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
+    // Step-7:
+    withdrawField.value = " ";
 
     // Step-3: 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    // Step-4: 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-
     // Step-5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Invalid');
+        return;
+    }
+
+    // Step-4: 
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     // Step-6:
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
